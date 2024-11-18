eed99fb6dd0242f01880d56be506be8ed5862b44
anhnguyen32 <anhnhp32@gmail.com>
2024-11-18 08:39:48 +0000

    5

--- app.py
+++ app.py
@@ -6,6 +6,11 @@ app = Flask(__name__)
 def index():
     return (render_template("index.html")) #backend
 
+@app.route("/",methods=['GET','POST']) 
+def main():
+    name = request.form.get("q")
+    return (render_template("main.html"))
+
 if __name__ == "__main__": #confirm 
     app.run()
 
